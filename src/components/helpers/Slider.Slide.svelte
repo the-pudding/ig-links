d8cbef62355858e0ee2efd1244777814351a7db4
jadiehm <jadiehm@gmail.com>
2020-12-16 09:36:34 -0600

    Initial commit

---
+++ src/components/helpers/Slider.Slide.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { getContext } from "svelte";
+  const { direction, width, height } = getContext("Slider");
+
+  $: w = $direction === "horizontal" ? $width : "100%";
+  $: h = $direction === "vertical" ? $height : "100%";
+</script>
+
+<div class="slide" style="width: {w}; height: {h};">
+  <slot />
+</div>
+
+<style>
+  .slide {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+</style>
