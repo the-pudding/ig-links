b15bf4eba8e93da0888a7d0ba3dd037603c3ced3
Jan Diehm <jadiehm@gmail.com>
2020-12-18 13:25:40 -0600

    adds meta

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -1,29 +1,31 @@
 <svelte:head>
-  <title>Svelte Starter</title>
+  <title>The Pudding's Instagram links</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <meta name="description" content="" />
   <meta name="news_keywords" content="" />
 
-  <meta property="og:title" content="" />
-  <meta property="og:site_name" content="" />
-  <meta property="og:url" content="" />
-  <meta property="og:description" content="description" />
+  <meta property="og:title" content="The Pudding's Instagram links" />
+  <meta property="og:site_name" content="The Pudding" />
+  <meta property="og:url" content="https://pudding.cool/projects/ig-links" />
+  <meta property="og:description" content="A collection of links from The Pudding's Instagram posts" />
   <meta property="og:type" content="article" />
   <meta property="og:locale" content="en_US" />
 
-  <meta property="og:image" content="" />
+  <meta property="og:image" 
+    content="https://pudding.cool/projects/ig-links/assets/images/social-facebook.jpg" />
   <meta property="og:image:type" content="image/jpeg" />
   <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="600" />
+  <meta property="og:image:height" content="628" />
 
   <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="" />
-  <meta name="twitter:creator" content="" />
-  <meta name="twitter:title" content="" />
-  <meta name="twitter:description" content="" />
-  <meta name="twitter:image:src" content="" />
+  <meta name="twitter:site" content="https://pudding.cool" />
+  <meta name="twitter:creator" content="@jadiehm" />
+  <meta name="twitter:title" content="The Pudding's Instagram links" />
+  <meta name="twitter:description" content="A collection of links from The Pudding's Instagram posts" />
+  <meta name="twitter:image:src" 
+    content="https://pudding.cool/projects/ig-links/assets/images/social-twitter.jpg" />
 
   <meta name="robots" content="max-image-preview:large" />
 
